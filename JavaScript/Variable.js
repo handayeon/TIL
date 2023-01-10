ca4d1024a1b6e4c759e6ac4d8cadbeedcd32027b
handayeon <gksekdus1212@naver.com>
2023-01-10 15:23:30 +0900

    :memo: javascript 변수 공부

---
+++ JavaScript/Variable.js
@@ -0,0 +1,14 @@
+// console.log("hello");
+
+// let a = 1;
+// console.log(a);
+
+//var - 한 번 선언된 변수를 다시 사용 가능
+var name = 'Mike';
+console.log(name);
+
+var name = 'Jane';
+console.log(name);
+
+//var - 함수스코프 (코드블럭 외에서도 사용가능)
+//let, const - 블록스코프 (코드블럭 내에서만 사용가능)
